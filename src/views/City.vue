fcfef501a81022e8084fe318c6b04adbd3ba0396
milkrong <milkrong121@outlook.com>
2020-09-18 20:18:41 +0800

    feat: city page search bar

--- src/views/City.vue
+++ src/views/City.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="city">
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from '@/components/City/Header'
+import CitySearch from '@/components/City/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
